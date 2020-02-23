8966f20b87076e62ee7dc24b7123227a57c538af
Noah Mutersbaugh <mutersbn@dc11.cis.gvsu.edu>
2020-02-22 20:04:52 -0500

    Adjust the cipher key to a 26 letter key

--- cipher.c
+++ cipher.c
@@ -9,7 +9,19 @@
 /* Included header files. */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "cipher.h"
+
+/*
+ * Description: Prepends t into s
+ */
+void prepend(char * s, char * t)
+{
+	size_t lengthT = strlen(t);
+	memmove(s + lengthT, s, strlen(s) + 1);
+	memcpy(s, t, lengthT);
+}
 
 /*
  * Function: RemoveDuplicates()
@@ -17,9 +29,56 @@
  * Return Type:
  * Description:
  */
-void RemoveDuplicates()
+void RemoveDuplicates(char * key, int sizeOfKey)
 {
+	const int SUPER_ALPHA_LENGTH = 27;
 	
+	int superAlphaExtendedLength = (sizeOfKey + SUPER_ALPHA_LENGTH);
+	int count = 0;
+
+	char superAlpha[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+		'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', '\0' };
+	char * superAlphaExt = (char*)malloc(superAlphaExtendedLength * sizeof(char));
+
+	prepend(superAlphaExt, superAlpha);
+	prepend(superAlphaExt, key);
+
+	printf("This is what's inside the extended array: %s\n", superAlphaExt);
+
+	//printf("Capitalizing all characters and removing duplicate chars...\n");
+	
+	/*
+	 * Lvl 1: Loop through each character in the array
+	 * Lvl 2: Compare character 'i' with each successive character
+	 * Lvl 3: Make duplicates spaces
+	 */
+	for(int i=0; i<superAlphaExtendedLength-1; i++)
+	{
+		if(superAlphaExt[i] >= 'a' && superAlphaExt[i] <= 'z')
+		{
+			superAlphaExt[i] = toupper(superAlphaExt[i]);
+		}
+		
+		for(int k=i+1; k<superAlphaExtendedLength; k++)
+		{
+			if(toupper(superAlphaExt[i]) == toupper(superAlphaExt[k]))
+			{
+				count++;
+				for(int n=k; n<superAlphaExtendedLength; n++)
+				{
+					superAlphaExt[n] = superAlphaExt[n+1];
+				}
+			}
+		}
+	}
+
+	// Reallocate memory so we aren't taking up more space than we should be.
+	superAlphaExt = (char*)realloc(superAlphaExt,
+			(superAlphaExtendedLength - count)*sizeof(char*));
+
+	printf("\nAfter capitalizing all characters and removing duplicates, the string is now %s\n",
+			superAlphaExt);
 }
 
 /*
--- main.c
+++ main.c
@@ -1,9 +1,33 @@
 #include <stdio.h>
+#include <string.h>
 #include "cipher.h"
 
-int main()
+int main(int argc, char * argv[])
 {
-	printf("Trying to access file...\n");
-	GetFile("THE_VOICE.txt");
+	char * cryptionDirection = argv[1];
+	char * wordKey = argv[2];
+	char * nameInFile = argv[3];
+	char * nameOutFile = argv[4];
+
+	int sizeOfKey = strlen(wordKey);
+
+	/* printf("This is what I read from the buffer:\n");
+	printf("%s %s %s %s\n", cryptionDirection, wordKey,
+			nameInFile, nameOutFile);
+*/
+	//printf("Trying to access file...\n");
+	//GetFile("THE_VOICE.txt");
+	
+	RemoveDuplicates(wordKey, sizeOfKey);
+	
+	if(*cryptionDirection == 'e')
+	{
+		/* Encrypt. */
+	}
+	else
+	{
+		/* Decrypt. */
+	}
+
 	return 0;
 }
